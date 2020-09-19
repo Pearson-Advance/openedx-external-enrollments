418ffb8d68bf25b1248de3501e5309ab64bfa767
Diego Millan <diego.millan@edunext.co>
2020-09-18 21:13:03 -0500

    Handling requirements in a better way Update requirements to work with python3.5 & juniper release

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@
 #
 ###############################################
 
+# Define PIP_COMPILE_OPTS=-v to get more information during make upgrade.
+PIP_COMPILE = pip-compile --rebuild --upgrade $(PIP_COMPILE_OPTS)
+
 .DEFAULT_GOAL := help
 
 help: ## display this help message
@@ -17,12 +20,13 @@ clean: ## delete most git-ignored files
 	find . -name '*~' -exec rm -f {} +
 
 requirements: ## install environment requirements
-	pip install -r requirements.txt
+	pip install -r requirements/base.txt
 
 upgrade: ## update the requirements/*.txt files with the latest packages satisfying requirements/*.in
-	pip install -q pip-tools
-	pip-compile --upgrade -o requirements/base.txt requirements/base.in
-	pip-compile --upgrade -o requirements/test.txt requirements/test.in
+	pip install -qr requirements/pip-tools.txt
+	$(PIP_COMPILE) -o requirements/pip-tools.txt requirements/pip-tools.in
+	$(PIP_COMPILE) -o requirements/base.txt requirements/base.in
+	$(PIP_COMPILE) -o requirements/test.txt requirements/test.in
 
 quality: clean ## check coding style with pycodestyle and pylint
 	pycodestyle ./openedx_external_enrollments
--- setup.py
+++ setup.py
@@ -25,6 +25,36 @@ def get_version(*file_paths):
 
 VERSION = get_version('openedx_external_enrollments', '__init__.py')
 
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line)
+        )
+    return list(requirements)
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, or editable.
+    """
+    # Remove whitespace at the start/end of the line
+    line = line.strip()
+
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+') or
+        line.startswith('-c')
+    )
 
 setup(
     name='openedx-external-enrollments',
@@ -35,12 +65,12 @@ setup(
     packages=[
         'openedx_external_enrollments'
     ],
-    include_package_data=True,
-    install_requires=[],
     zip_safe=False,
     entry_points={
         "lms.djangoapp": [
             'openedx_external_enrollments = openedx_external_enrollments.apps:OpenedxExternalEnrollmentConfig',
         ],
-    }
+    },
+    include_package_data=True,
+    install_requires=load_requirements('requirements/base.in')
 )
