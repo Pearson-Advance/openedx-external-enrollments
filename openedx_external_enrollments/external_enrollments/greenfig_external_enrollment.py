92c579ad75a13db936aa55812dd5a38eae403452
Javier <javier.torres@edunext.co>
2021-09-24 15:35:06 -0500

    Implement _post_enrollment for the Greenfig integration.

--- openedx_external_enrollments/external_enrollments/greenfig_external_enrollment.py
+++ openedx_external_enrollments/external_enrollments/greenfig_external_enrollment.py
@@ -98,3 +98,45 @@ class GreenfigInstanceExternalEnrollment(BaseExternalEnrollment):
             return str(error), status.HTTP_500_INTERNAL_SERVER_ERROR
         else:
             return response
+
+    def _post_enrollment(self, data, course_settings=None):
+        """
+        Get request data and execute the post request.
+        """
+        url = self._get_enrollment_url(course_settings)
+        json_data = self._get_enrollment_data(data, course_settings)
+        LOG.info('calling enrollment for [%s] with data: %s', self.__str__(), json_data)
+        LOG.info('calling enrollment for [%s] with url: %s', self.__str__(), url)
+        LOG.info('calling enrollment for [%s] with course settings: %s', self.__str__(), course_settings)
+        log_details = {
+            'request_payload': json_data,
+            'url': url,
+            'course_advanced_settings': course_settings,
+        }
+
+        try:
+            response = self._execute_post(
+                url=url,
+                headers=self._get_enrollment_headers(),
+                json_data=json_data,
+            )
+        except Exception as error:  # pylint: disable=broad-except
+            log_details['response'] = {'error': 'Failed to complete enrollment. Reason: %s' % str(error)}
+
+            LOG.error('Failed to complete enrollment. Reason: %s', str(error))
+            EnrollmentRequestLog.objects.create(  # pylint: disable=no-member
+                request_type=str(self),
+                details=log_details,
+            )
+            return str(error), status.HTTP_400_BAD_REQUEST
+        else:
+            json_response = self._get_json_response(response)
+            log_details['response'] = json_response
+
+            LOG.info('External enrollment response for [%s] -- %s', self.__str__(), json_response)
+            EnrollmentRequestLog.objects.create(  # pylint: disable=no-member
+                request_type=str(self),
+                details=log_details,
+            )
+
+            return json_response, status.HTTP_200_OK
